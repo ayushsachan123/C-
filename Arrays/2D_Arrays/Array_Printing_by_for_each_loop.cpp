9ff66eeed74e4ad5128a5e7cdcb3fe9daf2bb781
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-19 21:57:27 +0530

    Create Array_Printing_by_for_each_loop.cpp

---
+++ Arrays/2D_Arrays/Array_Printing_by_for_each_loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr1[] = {2,4,6};
+    for(int x:arr1)
+    {
+    cout<<x<<endl;
+    }
+    char arr2[] ={'A',66,'C',68};
+    for(auto y:arr2)
+    cout<<y<<endl;
+    return 0;
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Output--
+// 2
+// 4
+// 6
+// A
+// B
+// C
+// D
