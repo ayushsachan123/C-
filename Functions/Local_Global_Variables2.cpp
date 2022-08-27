ce69e668a0db21037aa02b003e7dd60b32367f40
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-27 23:03:15 +0530

    Create Local_Global_Variables2.cpp

---
+++ Functions/Local_Global_Variables2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int g=5;
+void fun(){
+    int g=10;
+    g++;
+}
+int main(){
+cout<<g<<endl;
+fun();
+cout<<g<<endl; 
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Output--
+// 5
+// 5
