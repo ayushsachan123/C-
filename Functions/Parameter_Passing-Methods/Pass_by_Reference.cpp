67e3e550c49a9c2d8865166adbae2e17a89b03b8
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-29 23:03:17 +0530

    Create Pass_by_Reference.cpp

---
+++ Functions/Parameter_Passing-Methods/Pass_by_Reference.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout<<"Output Of Swap Function"<<endl;
+    cout << a << " " << b<<endl;
+}
+int main()
+{
+    cout<<"Enter the Number you want to swap:- "<<endl;
+    int x, y;
+    cin >> x>>y;
+    swap(x, y);
+    cout<<"Output of main Function"<<endl;
+    cout << x << " " << y<<endl;
+    cout<<"There is interchange of value"<<endl;
+    cout<<"In pass by Reference"<<endl;
+    return 0;
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Input--
+// Enter the Number you want to swap:- 
+// 2 3
+// Output--
+// Output Of Swap Function
+// 3 2
+// Output of main Function
+// 3 2
+// There is interchange of value
+// In pass by Reference
