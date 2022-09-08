d2bf60bae7b8c1958d4b0950418a08142994b47d
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-09-08 23:14:57 +0530

    Create Ways_to_Inheritance_Using_Public.cpp

---
+++ Inheritance/Ways_to_Inheritance_Using_Public.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Parent{
+    private:int a;
+    protected: int b;
+    public:int c;
+    void funparent(){
+        a=10;
+        b=5;
+        c=15;
+    }
+};
+class Child:public Parent{
+    public:
+    void funchild(){
+        //a=10;
+        b=5;
+        c=15;
+
+}
+};
+class GrandChild:public Child{
+    public:
+    void funGrandchild(){
+    //a=10;
+    b=5;
+    c=20;
+    }
+};
+
+int main(){
+ Child c; 
+ //c.a=10; private & protected is not accessible in object
+ //c.b=5;
+ c.c=25;
+}
