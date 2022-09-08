8c123d91305abade6d6ee161f0fd7504a37e730a
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-09-08 23:11:54 +0530

    Create Ways_to_Inheritance_Using_Private.cpp

---
+++ Inheritance/Ways_to_Inheritance_Using_Private.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Parent{
+    private:int a;
+    protected: int b;
+    public:int c;
+    void funparent(){
+        a=10;
+        b=5;
+        c=15;
+    }
+};
+class Child:private Parent{
+    private:
+    protected:
+    public:
+    void funchild(){
+        //a=10;
+        b=5;
+        c=15;
+
+}
+};
+class GrandChild:public Child{
+    public:
+    void funGrandchild(){
+    //a=10;
+   // b=5; It cannot access private members
+   // c=20;
+    }
+};
+
+int main(){
+ Child c; 
+ //c.a=10; private & protected is not accessible in object
+ //c.b=5;
+ //c.c=25;
+}
+
+//-----------------------------------------------------------------------------------------------------------
