dfc320bda038e4e23c96f55dfe7eb6fde82ec1cc
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-09-09 23:04:14 +0530

    Create Creating_Object_In_Heap.cpp

---
+++ OOPs/Creating_Object_In_Heap.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Rectangle{
+  public:
+  int length;
+  int breadth;
+  int area(){
+    return length*breadth;
+  }
+  int perimeter(){
+    return 2*(length+breadth);
+  }
+};
+int main(){
+    Rectangle *ptr = new Rectangle();
+     ptr->length=10;
+   ptr->breadth=5;
+   cout<<ptr->area()<<endl;
+   cout<<ptr->perimeter()<<endl;
+
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Output--
+// 50
+// 30
