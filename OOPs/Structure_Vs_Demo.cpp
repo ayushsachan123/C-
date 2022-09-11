e5fbcaf9487e7b889f39939ddce392d47e8f9977
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-09-11 18:51:30 +0530

    Create Structure_Vs_Demo.cpp

---
+++ OOPs/Structure_Vs_Demo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+struct Demo{
+    int x;
+    int y;
+    void Display(){
+        cout<<x<<" "<<y<<endl;
+    }
+};
+int main(){
+Demo d;
+d.x=10;
+d.y=20;
+d.Display(); 
+}
+
+//-------------------------------------------------------------------------------------------------------------
+//Structure have everything is public by default but class have everything private by default
+// Output--
+// 10 20
