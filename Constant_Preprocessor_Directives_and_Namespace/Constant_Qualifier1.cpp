8bdcae0e56a3ac4387b8f42fafd82de71747acb9
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-09-26 22:53:05 +0530

    Create Constant_Qualifier1.cpp

---
+++ Constant_Preprocessor_Directives_and_Namespace/Constant_Qualifier1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const int x=10; //or int const x=10;
+       // x++; value of x cannot be changed
+       cout<<x;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// stant_Qualifier1
+// 10
