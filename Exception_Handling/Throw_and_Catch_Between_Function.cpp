da41e3b7b62dd7a0a5ac0c5997948eb3ce830efb
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-10-07 22:54:35 +0530

    Create Throw_and_Catch_Between_Function.cpp

---
+++ Exception_Handling/Throw_and_Catch_Between_Function.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int division(int a,int b){
+    if(b==0){
+        throw 1;
+        }
+      return a/b;
+}
+
+int main(){
+int x=10,y=0,z;
+try{
+z=division(x,y);
+cout<<z<<endl;
+}
+catch(int e){
+cout<<"Division by Zero "<<"error code: "<<e<<endl;
+}
+cout<<"Bye"<<endl;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// Bye
