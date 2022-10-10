60c2f394d9bb4d751b756f78f0094a4d66a4fa9d
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-10-10 16:47:25 +0530

    Create Demo_Static_Member1.cpp

---
+++ Friend_and-Static_Member_Inner_Class/Demo_Static_Member1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Innova{
+    public:
+    static int price;
+static int getprice(){
+    return price;
+}
+};
+int Innova::price = 20;
+int main(){
+    cout<<Innova::price<<endl;
+    Innova i1,i2,i3;
+    cout<<i1.price<<endl;
+    cout<<i2.price<<endl;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// 20
+// 20
+// 20
