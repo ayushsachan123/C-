3f0fec77d4da2bb09e3af8b775297c71b137b007
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-10-22 11:42:00 +0530

    Create Comperator1.cpp

---
+++ More_On_STL/Comperator/Comperator1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+ //sort(arr,arr+n); sorts everything in ascending order
+//If I want to sort in descending order
+//we have to write the function comperator
+//this comperator function has to be returninng true or false
+int arr[100];
+
+//just think of two element
+// bool comp(int el1,int el2){
+//     if(el1<=el2){
+//         return true;
+// }
+// return false;
+// }
+// bool comp(pair<int,int>el1,pair<int,int>el2){
+//     if(el1.first<el2.first){
+//         return true;
+//     }
+//     if(el1.first==el1.first){
+//         if(el1.second>el2.second){
+//             return true;
+//         }
+//     }
+//   return false;
+// }
+
+//arr
+//pair<int,int>arr[]={{1,4},{5,2},{5,9}};
+//after sorting arr[]={{1,4},{5,9},{5,2}};
+// sort(arr,arr+3,comp);
+//I want you to sort this in such a way
+//that the element who have first element in pair smaller
+//appears first, and if first is equal then sort according 
+//to second and keep the larger second
+
+
+    return 0;
+}
