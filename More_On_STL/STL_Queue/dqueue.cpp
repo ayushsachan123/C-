6a06e56bea5077dff81a50b0f2d36404aca6acee
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-11-03 14:15:03 +0530

    Create dqueue.cpp

---
+++ More_On_STL/STL_Queue/dqueue.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+deque<int> dq;
+
+//push_front()
+//push_back()
+//pop_front()
+//pop_back()
+//begin, end, rbegin, rend
+//size
+//clear
+//empty
+//add
+
+
+list<int>ls;
+
+//push_front()
+//push_back()
+//pop_front()
+//pop_back()
+//begin, end, rbegin, rend
+//size
+//clear
+//empty
+//add
+//remove -> 0(1)
+
+ls.push_front(1);
+ls.push_front(2);
+ls.push_front(3);
+
+ls.remove(2); //->o(1) operation 
+
+
+
+    return 0;
+}
