91805acc32e13cc2699b107f2971690fe530096a
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-11-03 14:13:58 +0530

    Create Stack2.cpp

---
+++ More_On_STL/Stack/Stack2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    stack<int>st;
+
+    //cout<<st.top(); throw an error because stack is empty
+    //so a;ways write the check function
+  
+  if(!st.empty()){
+    cout<<st.top();
+  }
+
+}
+
