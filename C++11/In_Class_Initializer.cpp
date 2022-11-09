6dec27f4faa11f02b506f69f4e49e5b74021cec8
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-11-09 22:48:48 +0530

    Create In_Class_Initializer.cpp

---
+++ C++11/In_Class_Initializer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+class Test{
+int x=10;
+int y=20;
+
+public:
+Test(int a,int b){
+    x=a;
+    y=b;
+}
+Test():Test(1,1){//It will call parametarized constructor
+                 //If you want to write the same logc in non-paramaterized
+                 //constructor so instead of writing again call parametized constructor
+
+}
+};
+
+int main(){
+
+    return 0;
+}
